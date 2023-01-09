ab724b086d82129ba97d157adb37342b43f28adf
Vanessa Vun <vanessavun@gmail.com>
2023-01-08 16:13:07 -0800

    Resume link edited

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
                     <a href="#contact">Contact</a>
                   </li>
                   <li className="mx-4">
-                    <a href="https://docs.google.com/document/d/1Az6FF484VaKT7OHEowfWajJ-hp5VQKXPvuyCamnrSeU/edit?usp=sharing" target="_blank">Resume</a>
+                    <a href="https://drive.google.com/file/d/1iNXdgLUF8Up_UMffX3qRT4aVozHki_CX/view?usp=share_link" target="_blank">Resume</a>
                   </li>
                 </ul>
               </div>
